49fc20b2246bbc45eaa04bd3ee0b3064b36011fc
FilipeGuto <filipeguto95@gmail.com>
2021-07-14 19:34:27 -0300

    Refatoraçao do script

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
+const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+const generatePokemons = () => Array(151).fill().map((_, index) =>
+  fetch(getPokemonUrl(index + 1)).then(response => response.json()))
+
 const fetchPokemon = () => {
-  const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
-  const pokemonsPromises = []
-  for(i = 1; i <= 151; i++) {
-    pokemonsPromises.push(fetch(getPokemonUrl(i)).then(response => response.json()))
-  }
+  const pokemonsPromises = generatePokemons()
 
   Promise.all(pokemonsPromises)
-  .then(pokemons => {
-    const lisPokemons = pokemons.reduce((acc, pokemon) => {
-      const types = pokemon.types.map(typeInfo => typeInfo.type.name)
-      acc += `
+    .then(pokemons => {
+      return pokemons = pokemons.reduce((acc, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+        acc += `
       <li class='card ${types[0]}'>
       <img class='card-image'  alt='${pokemon.name}' src= 'animated/${pokemon.id}.gif'/>
       <h2 class='card-title'>${pokemon.id}. ${pokemon.name}</h2>
       <p class='card-subtitle'>${types.join(' | ')}</p>
       </li>
       `
-      return acc
-    }, '')
-    const ul = document.querySelector('[data-js="pokedex"]')
-    ul.innerHTML = lisPokemons;
-  })
+        return acc
+      }, '')
+    })
+    .then(pokemons => {
+      const ul = document.querySelector('[data-js="pokedex"]')
+      ul.innerHTML = pokemons;
+    })
 }
 
 fetchPokemon();
